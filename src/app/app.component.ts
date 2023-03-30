a465918b25fb17c3d1f0094dc5b3fc69528689cf
Samuel Trejo <trejomsamuel@gmail.com>
2023-03-29 19:48:18 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,29 +3,155 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    <router-outlet></router-outlet>
+    <!-- <router-outlet></router-outlet> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="https://www.whatsapp.com/img/v4/logo-whatsapp.svg" alt="WhatsApp logo" width="40" height="40" class="d-inline-block align-text-top">
+          WhatsApp
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Download</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Security</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">FAQ</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- <div class="container mt-5">
+      <div class="row align-items-center">
+        <div class="col-md-6">
+          <h1 class="fw-bold">Simple. Secure.</h1>
+          <h2 class="fw-bold">Reliable messaging.</h2>
+          <p class="lead">With WhatsApp, you'll get fast, simple, secure messaging and calling for free*, available on phones all over the world.</p>
+          <button class="btn btn-primary">Download Now</button>
+        </div>
+        <div class="col-md-6">
+          <img src="https://www.whatsapp.com/img/v4/home-messaging-phone_2x.jpg" alt="WhatsApp messaging screenshot" class="img-fluid">
+        </div>
+      </div>
+    </div> -->
+
+    <!-- Hero section -->
+    <section class="hero py-3" style="background-image: url('https://source.unsplash.com/random/1920x1080')">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center justify-content-center">
+          <div class="col-md-8">
+            <h1 class="display-4 text-center mb-4">Stay in touch with your friends and family</h1>
+            <p class="lead text-center mb-5">WhatsApp is a fast, simple, and secure messaging and calling app that lets you connect with anyone, anywhere.</p>
+            <div class="d-flex justify-content-center">
+              <a class="btn btn-primary btn-lg" href="#" role="button">Get WhatsApp</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Main content section -->
+    <section class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Simple. Secure. Reliable messaging.</h2>
+            <p class="lead">With WhatsApp, you'll get fast, simple, secure messaging and calling for free*, available on phones all over the world.</p>
+            <p class="lead">*Data charges may apply. Contact your provider for details.</p>
+          </div>
+          <div class="col-md-6">
+            <img class="img-fluid" src="https://via.placeholder.com/600x400" alt="Smartphone with WhatsApp chat">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer class="mt-5 py-3 bg-light">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-md-6">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+              <li class="list-inline-item"><a href="#">Terms of Service</a></li>
+              <li class="list-inline-item"><a href="#">Brand Resources</a></li>
+            </ul>
+          </div>
+          <div class="col-md-6 text-md-end">
+            <p class="mb-0">© 2023 WhatsApp Inc.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
   `,
-  styles: []
+  styles: [
+    `/* Custom styles for the WhatsApp homepage component */
+
+    /* Hero styles */
+    .hero {}
+
+    /* Navbar styles */
+    .navbar {
+      background-color: #1e1e1e !important;
+      color: #fff;
+    }
+
+    .nav-link {
+      color: #fff !important;
+    }
+
+    /* Call to action button styles */
+    .btn-primary {
+      background-color: #ffc107;
+      border-color: #ffc107;
+    }
+
+    .btn-primary:hover {
+      background-color: #ffca2b;
+      border-color: #ffca2b;
+    }
+
+    /* Header text styles */
+    h1, h2 {
+      color: #ffc107;
+    }
+
+    /* Lead text styles */
+    .lead {
+      color: #fff;
+    }
+
+    /* Footer styles */
+    footer {
+      background-color: #333;
+      color: #fff;
+    }
+
+    /* Footer link styles */
+    ul.list-unstyled > li > a {
+      color: #ffc107;
+    }
+
+    ul.list-unstyled > li > a:hover {
+      color: #ffca2b;
+    }`
+  ]
 })
 export class AppComponent {
   title = 'roofing';
